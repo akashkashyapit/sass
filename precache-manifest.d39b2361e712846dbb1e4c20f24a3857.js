042605c39ad939edb0ed21c61a0e86f1d969b77c
akash <akashkashyapit@gmail.com>
2020-04-30 13:19:53 +0530

    websote

---
+++ precache-manifest.d39b2361e712846dbb1e4c20f24a3857.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "effe591f16c94de7852cdd079b3a6605",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0b1740ffd1012d725baa",
+    "url": "/static/css/2.8d06fd47.chunk.css"
+  },
+  {
+    "revision": "fcd73da055345985f9f0",
+    "url": "/static/css/main.dda74d2f.chunk.css"
+  },
+  {
+    "revision": "0b1740ffd1012d725baa",
+    "url": "/static/js/2.0086c69e.chunk.js"
+  },
+  {
+    "revision": "56729d980da09961afe8d497a21eec12",
+    "url": "/static/js/2.0086c69e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fcd73da055345985f9f0",
+    "url": "/static/js/main.d17b3868.chunk.js"
+  },
+  {
+    "revision": "1fddd770e8743a3f21e8",
+    "url": "/static/js/runtime-main.e25bea79.js"
+  }
+]);
